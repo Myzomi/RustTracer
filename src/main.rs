d2882cd72289352c89791baf9d736f35641e00ef
Myzomi <minhjtruong@gmail.com>
2024-10-06 21:05:28 -0400

    sphere with normal shading

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,24 @@ use color::*;
 use vec3::*;
 use ray::*;
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = *center - *r.origin();
     let a = r.direction().length_squared();
-    let half_b = dot(&oc, r.direction());
+    let b = -2.0 * dot(&oc, r.direction());
     let c = oc.length_squared() - radius * radius;
-    let discriminant = half_b * half_b - a * c;
-    discriminant > 0.0
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n: Vec3 = unit_vector(&(r.at(t) - Point3::new(0.0, 0.0, -1.0)));
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
 
     let unit_direction: Vec3 = unit_vector(&r.direction());
